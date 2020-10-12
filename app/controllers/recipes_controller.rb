418e4bb5b184f85d0410e4f49f90ff2cb882aa6a
Tomas Perez <tomasitopg6@gmail.com>
2020-10-12 23:15:16 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,6 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+    
     if @recipe.save
       redirect_to recipe_path(@recipe)
     else
