bbdf71e9c0a263ad2119cabce46cd1388dbc3b28
Tomas Perez <tomasitopg6@gmail.com>
2020-10-12 23:14:35 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: nil)
+    @recipe.ingredients.build(name: nil)
   end
 
   def create
